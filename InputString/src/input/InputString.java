2b2d6ffa55756e1bb6f95bfa30fb26afa2d36f34
alexa <alexa860@gmail.ru>
2025-07-23 19:00:13 +0300

    last

--- InputString/src/input/InputString.java
+++ InputString/src/input/InputString.java
@@ -11,31 +11,56 @@ public class InputString {
 		// TODO Auto-generated method stub
 		String inputText;
 		String close = "close";
+		String clearFile="clear";
 		String outputFile = "File.txt";
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer;
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-			
 			do {
+				writer = new BufferedWriter(new FileWriter(outputFile, true));
 				do {
 					try {
 						inputText=reader.readLine();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-						inputText=close;
+						inputText="Ошибка чтения ввода";
 					}
 				} while (inputText==null);
-				if(!inputText.equalsIgnoreCase(close)) {
+				
+				if(inputText.equalsIgnoreCase(close))writer.close();
+				else if(inputText.equalsIgnoreCase(clearFile)) {
+					System.out.println("Начало чистки");
+					writer.close();
+					clearFile(outputFile);
+				}
+				else if(!inputText.equalsIgnoreCase(close)) {
 					System.out.println("Вы напечатали " + inputText);
 					writer.write(inputText+" ");
+					writer.close();
 				}
+					
+				
 			} while(!inputText.equalsIgnoreCase(close));
-			writer.close();
 		} catch (IOException e) {
 			System.out.println("проблемы с записью в файл");
-		}	
+			inputText=close;	
+		}
+		}
 		
+	static BufferedWriter createWriter(String outputFile) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, true));
+		return writer;
+	}
+	static void clearFile(String outputFile) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+			System.out.println("чистка");
+			writer.write("");
+			writer.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
